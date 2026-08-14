c465cee36ea0be5349387337a722385f31a7a737
Guanqun Cao <gqcaoai@gmail.com>
2026-08-14 19:21:01 +0200

    Replace firefox with librewolf

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -2,7 +2,7 @@
 
 /* constants */
 #define TERMINAL "st"
-#define BROWSER "firefox"
+#define BROWSER "librewolf"
 
 /* appearance */
 static unsigned int borderpx  = 3;        /* border pixel of windows */
@@ -64,7 +64,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "librewolf",  NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
